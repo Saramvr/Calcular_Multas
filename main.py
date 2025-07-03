964756e56aa26b2a21d248e881d371a8f9c0f1f4
Saramvr <saramvr24@gmail.com>
2025-07-03 12:57:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,14 @@ def multa_fora_localidade(velocidade: float) -> int:
     elif velocidade > 90:
         return 60
     return 0
+
+def multa_autoestrada(velocidade: float) -> int:
+    if velocidade < 50:
+        return 0
+    elif velocidade > 175:
+        return 360
+    elif velocidade > 150:
+        return 120
+    elif velocidade > 120:
+        return 60
+    return 0
